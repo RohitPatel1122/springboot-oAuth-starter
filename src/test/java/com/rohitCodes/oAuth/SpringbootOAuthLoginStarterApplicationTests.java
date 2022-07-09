394a16214b0e7f3556d53c6faa4c9f4cfdc8436a
rohit <rohitpatel.1122@gmail.com>
2022-07-09 20:05:31 +0530

    Stage 1: App connecting to github and showing userName

---
+++ src/test/java/com/rohitCodes/oAuth/SpringbootOAuthLoginStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitCodes.oAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOAuthLoginStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
